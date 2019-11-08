90ade5116e6088424dc9df7f587b203f16c36de8
David Pascoe-Deslauriers <dpascoed@coldfrontlabs.ca>
2019-11-08 12:22:39 -0500

    feat (engines): make particle plumes face the correct direction

--- src/systems/particle.rs
+++ src/systems/particle.rs
@@ -62,30 +62,30 @@ impl<'s> System<'s> for EngineParticleSystem {
 
         for (ship, transform, mover, engine) in (&ships, &transforms, &movables, &engines).join() {
             if ship.applying_thrust != 0.0 {
-                let mut thrust = (ship.thrust * time.delta_seconds()) / 4.0;
+                let mut thrust = (ship.thrust * time.delta_seconds()) / 2.0;
                 if ship.applying_thrust > 0.0 {
                     thrust *= -1.0;
                 }
-                
+
                 let mut thrustvector = Vector3::new(0.0,thrust,0.0);
-                let angle = rng.gen_range(-10.0, 10.0);
+                let angle = rng.gen_range(-0.15, 0.15);
                 thrustvector = UnitQuaternion::from_euler_angles(0.0,0.0, angle).transform_vector(&thrustvector);
                 thrustvector = transform.rotation().transform_vector(&thrustvector);
-                
-
+                let mut pos = transform.clone();
+                pos.move_backward(-1.0);
                 let part: Entity = entities.create();
                 lazy_update.insert(part, sprite_sheet_manager.get_render("particles/particle0").unwrap());
-                lazy_update.insert(part, transform.clone());
+                lazy_update.insert(part, pos);
                 lazy_update.insert(part, Transparent);
                 lazy_update.insert(part, Tint(Srgba::new(1.0, 0.1, 0.1, 0.1)));
                 lazy_update.insert(part, Lifetime {
-                  start: time.absolute_real_time_seconds(),
-                  life: 0.25,
+                    start: time.absolute_real_time_seconds(),
+                    life: rng.gen_range(0.2, 0.3),
                 });
                 lazy_update.insert(part, Movable {
-                  velocity: mover.velocity + thrustvector,
-                  angular_velocity: 0.0,
-                  mass: 0.1,
+                    velocity: mover.velocity + thrustvector,
+                    angular_velocity: 0.0,
+                    mass: 0.1,
                 });
             }
 
