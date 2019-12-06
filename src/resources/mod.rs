01f92407a2375dd6c95266d5b6959b2799bb2c64
David Pascoe-Deslauriers <dpascoed@coldfrontlabs.ca>
2019-12-05 21:21:54 -0500

    feat (death): added death and respawn systems

--- src/resources/mod.rs
+++ src/resources/mod.rs
@@ -7,16 +7,16 @@ mod particle;
 mod ui;
 
 pub use self::{
-  assets::SpriteSheetManager,
-  collision::CollisionEvents,
-  collision::ForceCollision,
-  collision::TorpedoCollision,
-  damage::Damage,
-  damage::damage_types,
-  damage::DamageEvents,
-  particle::emit_particle,
-  explosions::generate_explosion,
-  math::unit_vector,
-  ui::StatusOfPlayer,
-  ui::StatusOfPlayers,
+    assets::SpriteSheetManager,
+    collision::CollisionEvents,
+    collision::ForceCollision,
+    collision::TorpedoCollision,
+    damage::Damage,
+    damage::damage_types,
+    damage::DamageEvents,
+    explosions::generate_explosion,
+    particle::emit_particle,
+    math::unit_vector,
+    ui::StatusOfPlayer,
+    ui::StatusOfPlayers,
 };
--- src/systems/player.rs
+++ src/systems/player.rs
@@ -1,5 +1,7 @@
 use amethyst::{
+    core::math::Vector3,
     core::timing::Time,
+    core::transform::Transform,
     core::SystemDesc,
     derive::SystemDesc,
     ecs::prelude::{Join, Read, ReadStorage, System, SystemData, World, WriteExpect, WriteStorage},
@@ -11,21 +13,57 @@ use crate::{
     resources::*,
 };
 
+use crate::{ARENA_HEIGHT, ARENA_WIDTH};
+
 #[derive(SystemDesc)]
 pub struct PlayerDeathSystem;
 
 impl<'s> System<'s> for PlayerDeathSystem {
     type SystemData = (
         Entities<'s>,
+        WriteStorage<'s, Transform>,
+        WriteStorage<'s, Movable>,
         WriteStorage<'s, Ship>,
         WriteStorage<'s, Player>,
+        ReadExpect<'s, SpriteSheetManager>,
+        ReadExpect<'s, LazyUpdate>,
+        Read<'s, Time>,
     );
 
     fn run(&mut self, (
         entities,
+        mut transforms,
+        mut movables,
         mut ships,
         mut players,
+        sprite_sheet_manager,
+        lazy_update,
+        time,
     ): Self::SystemData) {
+        for (transform, movable, ship, player) in (&mut transforms, &mut movables, &mut ships, &mut players).join() {
+            if ship.hull <= 0. {
+                player.dead = true;
+                player.respawn = time.absolute_real_time_seconds() + 3.;
+                transform.set_translation_xyz(0., 0., 20.0);
+                movable.angular_velocity = 0.;
+                movable.velocity = Vector3::new(0.,0.,0.);
+                generate_explosion(
+                    &transform,
+                    &movable,
+                    movable.mass,
+                    2.,
+                    150.,
+                    &entities,
+                    &sprite_sheet_manager,
+                    &lazy_update,
+                    &time,
+                    Explosion {
+                        vel: 150.,
+                        dsp: 10.,
+                    },
+                )
+            }
+        }
     }
 }
 
@@ -35,15 +73,37 @@ pub struct PlayerRespawnSystem;
 
 impl<'s> System<'s> for PlayerRespawnSystem {
     type SystemData = (
-        Entities<'s>,
+        WriteStorage<'s, Transform>,
+        WriteStorage<'s, Movable>,
+        WriteStorage<'s, Energy>,
         WriteStorage<'s, Ship>,
         WriteStorage<'s, Player>,
+        Read<'s, Time>,
     );
 
     fn run(&mut self, (
-        entities,
+        mut transforms,
+        mut movables,
+        mut energies,
         mut ships,
         mut players,
+        time,
     ): Self::SystemData) {
+        for (transform, movable, energy, ship, player) in (&mut transforms, &mut movables, &mut energies, &mut ships, &mut players).join() {
+            if player.dead && player.respawn >= time.absolute_real_time_seconds() {
+                player.dead = false;
+                ship.hull = 50.;
+                ship.shield = 75.;
+                energy.charge= 100.;
+                movable.angular_velocity = 0.6;
+                if player.id == 1 {
+                    transform.set_translation_xyz(ARENA_WIDTH/4.0, ARENA_HEIGHT/2.0, 0.0);
+                    movable.velocity = Vector3::new(0.,120.,0.);
+                } else if player.id == 2 {
+                    transform.set_translation_xyz(3.0*(ARENA_WIDTH/4.0), ARENA_HEIGHT/2.0, 0.0);
+                    movable.velocity = Vector3::new(0.,-120.,0.);
+                }
+            }
+        }
     }
 }
