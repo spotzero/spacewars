33a03f34e65f98938907edae3d57a59aeaf57245
David Pascoe-Deslauriers <dpascoed@coldfrontlabs.ca>
2020-03-04 08:02:21 -0500

    more work on menu

--- src/states/menu.rs
+++ src/states/menu.rs
@@ -1,5 +1,7 @@
 use amethyst::{
+    core::math::Vector3,
     core::timing::Time,
+    core::transform::Transform,
     input::{is_close_requested, is_key_down, VirtualKeyCode},
     prelude::*,
     GameData, SimpleState, SimpleTrans, StateData, Trans,
@@ -7,13 +9,17 @@ use amethyst::{
 
 use crate::resources::*;
 use crate::states::*;
+use crate::{ARENA_HEIGHT, ARENA_WIDTH};
+
 
 pub struct MenuState;
 
 impl SimpleState for MenuState {
     fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
         data.world.fetch_mut::<Game>().current_state = CurrentState::Menu;
-        data.world.fetch_mut::<Time>().set_time_scale(0.);
+        data.world.fetch_mut::<Time>().set_time_scale(1.);
+
+        init_menu(data.world);
     }
 
     fn handle_event(
@@ -35,4 +41,39 @@ impl SimpleState for MenuState {
         // Keep going
         Trans::None
     }
+}
+
+fn init_menu(world: &mut World) {
+    let bg_ss = world
+        .fetch::<AssetManager>()
+        .get_render("backgrounds/background-2")
+        .unwrap();
+
+    let title = world
+        .fetch::<AssetManager>()
+        .get_render("logo")
+        .unwrap();
+
+    let controls = world
+        .fetch::<AssetManager>()
+        .get_render("controls")
+        .unwrap();
+
+
+    let mut bg_transform = Transform::default();
+    let scale = ARENA_WIDTH / 1000.0;
+    bg_transform.set_translation_xyz(ARENA_WIDTH / 2.0, ARENA_HEIGHT / 2.0, -10.0);
+    bg_transform.set_scale(Vector3::new(scale, scale, scale));
+    world.create_entity().with(bg_ss).with(bg_transform).build();
+
+    let mut title_transform = Transform::default();
+    bg_transform.set_translation_xyz(ARENA_WIDTH / 2.0, ARENA_HEIGHT / 2.0, -10.0);
+    bg_transform.set_scale(Vector3::new(scale, scale, scale));
+    world.create_entity().with(bg_ss).with(bg_transform).build();
+
+        let mut bg_transform = Transform::default();
+    let scale = ARENA_WIDTH / 1000.0;
+    bg_transform.set_translation_xyz(ARENA_WIDTH / 2.0, ARENA_HEIGHT / 2.0, -10.0);
+    bg_transform.set_scale(Vector3::new(scale, scale, scale));
+    world.create_entity().with(bg_ss).with(bg_transform).build();
 }
